cbc2ea48b17f3d760866a46c9ced9da24dca9035
SteveC <cheema35@hotmail.com>
2020-02-27 13:02:39 -0800

    plugin loaded

---
+++ themes/inhabitent-master/page-about.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Template displaying page content for about page.php.
+ *
+ * @package Inhabitent_Theme
+ */
+
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+			TEST PAGE
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+
+			<?php endwhile; // End of the loop. ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
