cc0577b122410f8a926413d52d1fbe92f604173c
SteveC <cheema35@hotmail.com>
2020-02-27 16:44:57 -0800

    second widgit

--- themes/inhabitent-master/about.php
+++ themes/inhabitent-master/about.php
@@ -1,16 +1,17 @@
 <?php
-/**
+	/**
  * Template displaying page content for about page.php.
  *
  * @package Inhabitent_Theme
  */
 
 
-get_header(); ?>
+ get_header(); ?>
+
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-			TEST PAGE
+		
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<?php get_template_part( 'template-parts/content', 'page' ); ?>
